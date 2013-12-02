37388951255c8d835f4b943a2ca59e713495c173
Kevin Wallace <kevin@pentabarf.net>
2013-12-01 16:43:34 -0800

    Make the device node in the Dockerfile, rather than at run-time.
    This depends on dotcloud/docker#2979.

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,9 @@ RUN apt-get upgrade -y
 
 RUN apt-get install -y qemu-kvm
 
+RUN mknod /dev/kvm c 10 232
+
 RUN mkdir -p /root/qemu
-ADD kvm-mknod.sh /root/qemu/kvm-mknod.sh
 ADD entrypoint.sh /root/qemu/entrypoint.sh
 RUN chmod +x /root/qemu/*.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 # Docker entrypoint that does runtime setup of the container environment for KVM,
 # before running the user-specified command.
 
-/root/qemu/kvm-mknod.sh
+dd if=/dev/kvm count=0 2>/dev/null || {
+  echo >&2 "Unable to open /dev/kvm; qemu will use software emulation"
+  echo >&2 "(This can happen if the container is run without -privileged," \
+           "or if the KVM kernel module is not loaded)"
+}
+
 exec "$@"
